ae8537ae03b3dc6281e73d4017cbd0549e12d998
Yekpahi Prince <princeyekpahi@gmail.com>
2023-02-25 16:45:06 +0100

    First commit

---
+++ Array/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    var arr1  = arrayOf(1, 10, 4, 6, "Abidjan");
+
+    println("${arr1[0]}")
+    val get = arr1.get(2)
+    print("Ici : $get");
+}
---
+++ Array/tempCodeRunnerFile.kt
@@ -0,0 +1,5 @@
+ calc(a: Int, b : Int) : Int {
+    var sum : Int;
+    sum = a + b;
+    return sum;
+}
