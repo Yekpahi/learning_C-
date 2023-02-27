d987f5d916e079240481b088fb8bf23551b861c1
Yekpahi Prince <princeyekpahi@gmail.com>
2023-02-27 15:36:34 +0100

    iteration

---
+++ iterations/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val names = listOf("Jake Wharton", "Joe Birch", "Robert Martin");
+    /*
+       for (i in names.indices) {
+        println(": $i is ${names[i]}")
+    }
+    */
+ for (i in 1 .. names.size) {
+    println(i)
+ }
+
+}
