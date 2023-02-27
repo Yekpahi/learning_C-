3f649409b8b17a8d0a4a6214d684eaee26c2f1e3
Yekpahi Prince <princeyekpahi@gmail.com>
2023-02-27 15:01:14 +0100

    Enum

--- Conditions/main.kt
+++ Conditions/main.kt
@@ -14,8 +14,8 @@ fun main() {
     }
     println("Le resultat2 : $result2")
     */
-
-    val aPirest = 200
+/*
+ val aPirest = 200
 
     when(aPirest) {
         200, 201, 202 -> println("OK")
@@ -23,4 +23,46 @@ fun main() {
         500 -> println("Man you're foul")
         else -> println("Unknown")
     }
+*/
+/*
+ for (i in ApiResponse.values()) {
+    println("${i.ordinal} = ${i.name} and status = ${i.status}")
+ }
+*/
+
+seasons.Rainy.foo()
+
 }
+/*
+enum class ApiResponse(val status :Int = 120) {
+    OK(200),
+    NOT_FOUND,
+    UNAUTHORIZED,
+    FORBIDEN,
+    UNKNOWN,
+}
+
+*/
+
+enum class seasons (var weather : String) {
+
+Summer("Hot") {
+    override fun foo() {
+        println("C'est l'ete")
+    }
+},
+
+Winter("Cold") {
+    override fun foo() {
+        println("C'est l'hiver")
+    }
+},
+
+Rainy("Moderate") {
+    override fun foo() {
+        println("C'est l'autome")
+    }
+};
+abstract fun foo()
+}
+
